ebb5553807679d10114cdb39dd9b5f935e3d2f71
Руслан Жевраков <116262017+RyslanZevrakov@users.noreply.github.com>
2024-06-15 15:28:47 +0300

    Add files via upload

---
+++ main/java/com/example/bookstoreapp/data/Book.kt
@@ -0,0 +1,11 @@
+package com.example.bookstoreapp.data
+
+
+
+data class Book(
+    val name:String = "",
+    val descriptor: String = "",
+    val price: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+)
